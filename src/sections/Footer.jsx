379b1b41cb7ee2a5f90f0aec4b9b718d08163cea
Ormon-huehuehue <armnvrma10@gmail.com>
2024-06-12 13:46:42 +0530

    added social media icons in footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { footerLogo } from '../assets/images'
+import { socialMedia } from '../constants'
 
 const Footer = () => {
   return (
@@ -10,6 +11,13 @@ const Footer = () => {
             <img src={footerLogo} width = {150} height = {46}/>
           </a>
           <p className = "font-montserrat text-slate-gray mt-5">Get shoes ready at the nearest Nike store</p>
+          <div className = "flex items-center gap-5 mt-8">
+            {socialMedia.map((icon)=>(
+              <div className = "flex justify-center items-center w-12 h-12 bg-white rounded-full">
+                <img src ={icon.src} alt = {icon.alt} width={24} height = {24}/>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
